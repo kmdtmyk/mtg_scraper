c76a7efa60c9fb53411701a5665185dfe84899be
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-01-29 16:47:15 +0900

    Reduce line breaks

--- wisdom_guild_test.rb
+++ wisdom_guild_test.rb
@@ -14,19 +14,11 @@ class WisdomGuildTest < Test::Unit::TestCase
           mana_cost: '(４)(緑)',
           legendary: false,
           types: [
-            {
-              name: 'クリーチャー',
-            },
+            { name: 'クリーチャー' },
           ],
           subtypes: [
-            {
-              name: 'トカゲ',
-              english_name: 'Lizard',
-            },
-            {
-              name: 'ビースト',
-              english_name: 'Beast',
-            },
+            { name: 'トカゲ', english_name: 'Lizard' },
+            { name: 'ビースト', english_name: 'Beast' },
           ],
           text: "進化（クリーチャー１体があなたのコントロール下で戦場に出るたび、そのクリーチャーのパワーかタフネスがこのクリーチャーよりも大きい場合、このクリーチャーの上に+1/+1カウンターを１個置く。）",
           oracle: "Evolve （Whenever a creature enters the battlefield under your control, if that creature has greater power or toughness than this creature, put a +1/+1 counter on this creature.）",
@@ -46,15 +38,10 @@ class WisdomGuildTest < Test::Unit::TestCase
           mana_cost: '(３)(赤)',
           legendary: true,
           types: [
-            {
-              name: 'クリーチャー',
-            },
+            { name: 'クリーチャー' },
           ],
           subtypes: [
-            {
-              name: '神',
-              english_name: 'God',
-            },
+            { name: '神', english_name: 'God' },
           ],
           text: "破壊不能、速攻\nあなたの手札のカードが１枚以下でないかぎり、熱烈の神ハゾレトでは攻撃したりブロックしたりできない。\n(２)(赤),カード１枚を捨てる：熱烈の神ハゾレトは各対戦相手にそれぞれ２点のダメージを与える。",
           oracle: "Indestructible, haste\nHazoret the Fervent can't attack or block unless you have one or fewer cards in hand.\n{2}{R}, Discard a card: Hazoret deals 2 damage to each opponent.",
@@ -99,9 +86,7 @@ class WisdomGuildTest < Test::Unit::TestCase
           mana_cost: '(１)(青)(青)',
           legendary: false,
           types: [
-            {
-              name: 'インスタント',
-            },
+            { name: 'インスタント' },
           ],
           subtypes: [],
           text: "呪文１つを対象とし、それを打ち消す。",
@@ -121,9 +106,7 @@ class WisdomGuildTest < Test::Unit::TestCase
           mana_cost: '(６)(赤)(赤)',
           legendary: false,
           types: [
-            {
-              name: 'ソーサリー',
-            },
+            { name: 'ソーサリー' },
           ],
           subtypes: [],
           text: "対戦相手１人がコントロールするクリーチャー１体を対象とする。それは自身のパワーに等しい点数のダメージを、そのプレイヤーがコントロールする他の各クリーチャーにそれぞれ与える。その後、それらの各クリーチャーは、それぞれのパワーに等しい点数のダメージをそのクリーチャーに与える。",
@@ -143,15 +126,10 @@ class WisdomGuildTest < Test::Unit::TestCase
           mana_cost: '(２)(白)',
           legendary: false,
           types: [
-            {
-              name: 'エンチャント',
-            },
+            { name: 'エンチャント' },
           ],
           subtypes: [
-            {
-              name: 'オーラ',
-              english_name: 'Aura',
-            },
+            { name: 'オーラ', english_name: 'Aura' },
           ],
           text: "エンチャント（クリーチャー）\nエンチャントされているクリーチャーは攻撃もブロックもできず、その起動型能力を起動できない。",
           oracle: "Enchant creature\nEnchanted creature can't attack or block, and its activated abilities can't be activated.",
@@ -170,9 +148,7 @@ class WisdomGuildTest < Test::Unit::TestCase
           mana_cost: '(７)',
           legendary: true,
           types: [
-            {
-              name: 'アーティファクト',
-            },
+            { name: 'アーティファクト' },
           ],
           subtypes: [],
           text: "あなたがコントロールするクリーチャーは飛行、先制攻撃、警戒、トランプル、速攻、プロテクション（黒）とプロテクション（赤）を持つ。",
