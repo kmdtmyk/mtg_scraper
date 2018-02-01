deeba91f44dfbd7c8686a189dbab9ede17a52a0b
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-02-01 13:44:48 +0900

    Merge files into one

--- wisdom_guild.rb
+++ wisdom_guild.rb
@@ -1,6 +1,5 @@
+require 'open-uri'
 require 'nokogiri'
-require_relative 'html_man'
-require_relative 'file_man'
 
 class WisdomGuild
 
@@ -15,12 +14,12 @@ class WisdomGuild
 
   def self.get_html_and_cache(name)
     file_path = name_to_file_path(name)
-    html = FileMan.read(file_path)
+    html = FileUtil.read(file_path)
     return html unless html.nil?
 
     url = name_to_url(name)
-    html = HtmlMan.get(url)
-    FileMan.write(file_path, html)
+    html = HtmlUtil.get(url)
+    FileUtil.write(file_path, html)
     html
   end
 
@@ -113,146 +112,177 @@ class WisdomGuild
     return 'normal'
   end
 
-  private
+  def self.get_table(html)
+    doc = Nokogiri::HTML.parse(html)
+    doc.css('.wg-whisper-card-detail table')
+  end
 
-    def self.get_table(html)
-      doc = Nokogiri::HTML.parse(html)
-      doc.css('.wg-whisper-card-detail table')
+  def self.table_to_array(table)
+    table.css('tr').map do |tr|
+      tr.css('th, td').map{ |td| td.inner_text }
     end
+  end
 
-    def self.table_to_array(table)
-      table.css('tr').map do |tr|
-        tr.css('th, td').map{ |td| td.inner_text }
+  def self.parse_double_faced_or_flip(html)
+    table = get_table(html)
+    text_array = table_to_array(table)
+    second_name_index = text_array[1..-1].index{ |name, value| name == 'カード名' } + 1
+    [
+      parse_text_array(text_array[0..second_name_index - 1]),
+      parse_text_array(text_array[second_name_index..-2]),
+    ]
+  end
+
+  def self.parse_name_text(text)
+    %r{([^/\n\t]+)\/([^/\n\t]+)[\n\t]*（(.+)）}.match(text)
+    name = Regexp.last_match(1)
+    english_name = Regexp.last_match(2)
+    furigana = Regexp.last_match(3)
+    {
+      name: name,
+      english_name: english_name,
+      furigana: furigana,
+    }
+  end
+
+  def self.parse_text_array(array)
+    result = {}
+    array.each do |name, value|
+      if name == 'カード名'
+        result.merge!(parse_name_text(value))
+      elsif name == 'マナコスト'
+        result[:mana_cost] = value
+      elsif name == 'タイプ'
+        result.merge!(parse_type_text(value))
+      elsif name == 'テキスト'
+        result[:text] = value
+      elsif name == 'オラクル'
+        result[:oracle] = value
+      elsif name == 'Ｐ／Ｔ'
+        result.merge!(parse_size_text(value))
+      elsif name == '忠誠度'
+        result.merge!(parse_loyalty_text(value))
+      elsif name == 'フレーバ'
+        result[:flavor_text] = value
+      elsif name == 'デザイン'
+      elsif name == 'イラスト'
+        result[:artist] = value
+      elsif name == 'セット等'
+      elsif name == '再録'
+      end
+    end
+    result
+  end
+
+  def self.parse_type_text(text)
+    %r{(〔(.*)〕 )?(伝説の|基本|氷雪|持続|精鋭|宿主|ワールド・)*(\S+)( — (\S+))?}.match(text)
+    color_text = Regexp.last_match(2)
+    supertype_text = Regexp.last_match(3)
+    type_text = Regexp.last_match(4)
+    subtype_text = Regexp.last_match(6)
+
+    supertypes = []
+    types = []
+    subtypes = []
+
+    supertypes << { name: '伝説の' } if %r{伝説の}.match(text)
+    supertypes << { name: '基本' } if %r{基本}.match(text)
+    supertypes << { name: '氷雪' } if %r{氷雪}.match(text)
+    supertypes << { name: 'ワールド' } if %r{ワールド}.match(text)
+
+    unless type_text.nil?
+      type_text.split('・').each do |type|
+        types << {name: type}
       end
     end
 
-    def self.parse_double_faced_or_flip(html)
-      table = get_table(html)
-      text_array = table_to_array(table)
-      second_name_index = text_array[1..-1].index{ |name, value| name == 'カード名' } + 1
-      [
-        parse_text_array(text_array[0..second_name_index - 1]),
-        parse_text_array(text_array[second_name_index..-2]),
-      ]
-    end
-
-    def self.parse_name_text(text)
-      %r{([^/\n\t]+)\/([^/\n\t]+)[\n\t]*（(.+)）}.match(text)
-      name = Regexp.last_match(1)
-      english_name = Regexp.last_match(2)
-      furigana = Regexp.last_match(3)
-      {
-        name: name,
-        english_name: english_name,
-        furigana: furigana,
-      }
-    end
-
-    def self.parse_text_array(array)
-      result = {}
-      array.each do |name, value|
-        if name == 'カード名'
-          result.merge!(parse_name_text(value))
-        elsif name == 'マナコスト'
-          result[:mana_cost] = value
-        elsif name == 'タイプ'
-          result.merge!(parse_type_text(value))
-        elsif name == 'テキスト'
-          result[:text] = value
-        elsif name == 'オラクル'
-          result[:oracle] = value
-        elsif name == 'Ｐ／Ｔ'
-          result.merge!(parse_size_text(value))
-        elsif name == '忠誠度'
-          result.merge!(parse_loyalty_text(value))
-        elsif name == 'フレーバ'
-          result[:flavor_text] = value
-        elsif name == 'デザイン'
-        elsif name == 'イラスト'
-          result[:artist] = value
-        elsif name == 'セット等'
-        elsif name == '再録'
-        end
+    unless subtype_text.nil?
+      subtype_text.split('・').each do |subtype|
+        %r{(\S+)\((\S+)\)}.match(subtype)
+        subtypes << {
+          name: Regexp.last_match(1),
+          english_name: Regexp.last_match(2),
+        }
       end
-      result
     end
 
-    def self.parse_type_text(text)
-      %r{(〔(.*)〕 )?(伝説の|基本|氷雪|持続|精鋭|宿主|ワールド・)*(\S+)( — (\S+))?}.match(text)
-      color_text = Regexp.last_match(2)
-      supertype_text = Regexp.last_match(3)
-      type_text = Regexp.last_match(4)
-      subtype_text = Regexp.last_match(6)
+    result = {
+      supertypes: supertypes,
+      types: types,
+      subtypes: subtypes,
+    }
 
-      supertypes = []
-      types = []
-      subtypes = []
-
-      supertypes << { name: '伝説の' } if %r{伝説の}.match(text)
-      supertypes << { name: '基本' } if %r{基本}.match(text)
-      supertypes << { name: '氷雪' } if %r{氷雪}.match(text)
-      supertypes << { name: 'ワールド' } if %r{ワールド}.match(text)
-
-      unless type_text.nil?
-        type_text.split('・').each do |type|
-          types << {name: type}
-        end
+    unless color_text.nil?
+      result[:colors] = color_text.split('/').map do |color|
+        { name: color }
       end
+    end
 
-      unless subtype_text.nil?
-        subtype_text.split('・').each do |subtype|
-          %r{(\S+)\((\S+)\)}.match(subtype)
-          subtypes << {
-            name: Regexp.last_match(1),
-            english_name: Regexp.last_match(2),
-          }
-        end
+    result
+  end
+
+  def self.parse_size_text(text)
+    %r{(.*)/(.*)}.match(text)
+    power = Regexp.last_match(1).to_i
+    toughness = Regexp.last_match(2).to_i
+    {
+      size: text,
+      power: power,
+      toughness: toughness,
+    }
+  end
+
+  def self.parse_loyalty_text(text)
+    {
+      size: text,
+      loyalty: text.to_i,
+    }
+  end
+
+  def self.parse_level_text(text)
+    %r{Lv(\S*)}.match(text)
+    Regexp.last_match(1)
+  end
+
+  def self.name_to_url(name)
+    return 'http://whisper.wisdom-guild.net/card/' + URI.escape(name)
+  end
+
+  def self.name_to_file_path(name)
+    File.dirname(__FILE__) + "/cache/#{name}.html"
+  end
+
+  class FileUtil
+
+    def self.read(path)
+      begin
+        return File.read(path)
+      rescue Exception => e
+        return nil
       end
+    end
 
-      result = {
-        supertypes: supertypes,
-        types: types,
-        subtypes: subtypes,
-      }
-
-      unless color_text.nil?
-        result[:colors] = color_text.split('/').map do |color|
-          { name: color }
-        end
+    def self.write(path, text)
+      dirname = File.dirname(path)
+      unless Dir.exist?(dirname)
+        Dir.mkdir(dirname)
       end
-
-      result
+      File.open(path, 'w') do |f|
+        f.puts(text)
+      end
     end
 
-    def self.parse_size_text(text)
-      %r{(.*)/(.*)}.match(text)
-      power = Regexp.last_match(1).to_i
-      toughness = Regexp.last_match(2).to_i
-      {
-        size: text,
-        power: power,
-        toughness: toughness,
-      }
+  end
+
+  class HtmlUtil
+
+    def self.get(url)
+      html = open(url) do |f|
+        f.read
+      end
+      html
     end
 
-    def self.parse_loyalty_text(text)
-      {
-        size: text,
-        loyalty: text.to_i,
-      }
-    end
-
-    def self.parse_level_text(text)
-      %r{Lv(\S*)}.match(text)
-      Regexp.last_match(1)
-    end
-
-    def self.name_to_url(name)
-      return 'http://whisper.wisdom-guild.net/card/' + URI.escape(name)
-    end
-
-    def self.name_to_file_path(name)
-      File.dirname(__FILE__) + "/cache/#{name}.html"
-    end
+  end
 
 end
