d95e563399a10caa324557fe7494f51865fdfce9
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-08-29 06:58:43 +0900

    Remove unnecessary constants

--- lib/mtg_scraper/hareruya.rb
+++ lib/mtg_scraper/hareruya.rb
@@ -2,8 +2,4 @@ require 'mtg_scraper/hareruya/list'
 
 module MtgScraper
 
-  module Hareruya
-    NAME = 'hareruya'
-  end
-
 end
--- lib/mtg_scraper/wisdom_guild.rb
+++ lib/mtg_scraper/wisdom_guild.rb
@@ -2,8 +2,4 @@ require 'mtg_scraper/wisdom_guild/card'
 
 module MtgScraper
 
-  module WisdomGuild
-    NAME = 'wisdom_guild'
-  end
-
 end
