9fdf21a356f7b97d8e595af8b509cb1b08409dbe
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-09-03 21:33:43 +0900

    Implement methods for card set

--- lib/mtg_scraper/mtgjson/set.rb
+++ lib/mtg_scraper/mtgjson/set.rb
@@ -1,12 +1,35 @@
 # frozen_string_literal: true
 
+require 'json'
+
 module MtgScraper::Mtgjson
 
   class Set
+    include Enumerable
 
     def initialize(text)
       @text = text
     end
+
+    def each
+      json[:cards].each do |card|
+        yield card
+      end
+    end
+
+    def size
+      json[:cards].size
+    end
+
+    def [](nth)
+      json[:cards][nth]
+    end
+
+    private
+
+      def json
+        JSON.parse(@text, symbolize_names: true)
+      end
 
   end
 
---
+++ spec/mtg_scraper/mtgjson/set_spec.rb
@@ -0,0 +1,30 @@
+RSpec.describe MtgScraper::Hareruya::List do
+
+  let(:page){ page = MtgScraper::Page.new(url) }
+  let(:html){ page.html }
+  let(:set){ MtgScraper::Mtgjson::Set.new(html) }
+
+  describe '#size' do
+
+    subject{ set.size }
+
+    context do
+      let(:url){ 'http://mtgjson.com/json/M19.json' }
+      it{ expect(subject).to eq 315 }
+    end
+
+  end
+
+  describe '#[]' do
+
+    subject{ set[nth] }
+
+    context do
+      let(:url){ 'http://mtgjson.com/json/M19.json' }
+      let(:nth){ 0 }
+      it{ expect(subject[:name]).to eq "Aegis of the Heavens" }
+    end
+
+  end
+
+end
