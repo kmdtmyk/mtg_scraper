bda6694fb305a129d37e467f419d4da763f64bd1
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-09-04 22:00:34 +0900

    Change class api

--- lib/mtg_scraper/wisdom_guild/card.rb
+++ lib/mtg_scraper/wisdom_guild/card.rb
@@ -12,11 +12,8 @@ module MtgScraper
 
     class Card
 
-      @@interval = 5
-      @@last_time = nil
-
-      def initialize(name)
-        @url = 'http://whisper.wisdom-guild.net/card/' + URI.escape(name)
+      def initialize(html)
+        @html = html
       end
 
       def name
@@ -39,16 +36,6 @@ module MtgScraper
         @details
       end
 
-      def html
-        return @html unless @html.nil?
-        return read_cache if cached?
-        fetch_html
-      end
-
-      def cached?
-        MtgScraper::Cache.exist?(@url)
-      end
-
       def error?
         return @error unless @error.nil?
         parse_html
@@ -67,32 +54,13 @@ module MtgScraper
       private
 
         def parse_html
-          result = ParseHtml.parse(html)
+          result = ParseHtml.parse(@html)
           @error = result[:error]
           return if @error
           @name = result[:name]
           @english_name = result[:english_name]
           @multiverseid = result[:multiverseid]
           @details = result[:details].map{ |detail| Detail.new(detail) }
-        end
-
-        def read_cache
-          @html = MtgScraper::Cache.read(@url)
-        end
-
-        def write_cache
-          MtgScraper::Cache.write(@url, @html)
-        end
-
-        def fetch_html
-          unless @@last_time.nil?
-            sleep_time = @@interval - (Time.now - @@last_time)
-            sleep sleep_time if 0 < sleep_time
-          end
-          @html = MtgScraper::Utils::HtmlUtil.get(@url)
-          @@last_time = Time.now
-          write_cache
-          @html
         end
 
     end
